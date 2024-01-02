0603701860e5488c6c73fcdc9431b94bcb8de6fd
Naidaly ruiz <naidalyruiz@gmail.com>
2024-01-02 12:17:52 +0100

    test

--- src/js/form.js
+++ src/js/form.js
@@ -1,2 +1,38 @@
 'Use strict';
 
+section.addEventListener("click",(event)=>{
+    event.preventDefault();
+
+    if(section.classList.contains('hidden')){
+        section.classList.toggle('hidden')
+    }
+     else(
+        section.classList.tog('hidden')
+    );
+});
+
+//lo mismo pero con el form//
+
+form.addEventListener("click",(event)=>{
+    event.preventDefault();
+
+    if(form.classList.contains('hidden')){
+        form.classList.remove('hidden')
+    }
+     else(
+        form.classList.add('hidden')
+    );
+});
+
+//Y más con el button de share//
+
+btnShare.addEventListener("click",(event)=>{
+    event.preventDefault();
+
+    if(btnShare.classList.contains('hidden')){
+        btnShare.classList.remove('hidden')
+    }
+     else(
+        btnShare.classList.add('hidden')
+    );
+});
