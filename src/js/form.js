9d704a1947dc5215626e28748df72193cb980b56
Wilanda2 <yolanda.lopezdelarica@gmail.com>
2024-01-10 12:57:38 +0100

    Fix arrows up

--- src/js/form.js
+++ src/js/form.js
@@ -1,26 +1,59 @@
 'use strict';
 
 function handleHiddenDesign(event) {
-    section.classList.toggle('hidden');
-    arrow.classList.toggle('arrowUp');
+    if (section.classList.contains('hidden')) {
+        section.classList.remove('hidden');
+        formContainer.classList.add('hidden');
+        btnShare.classList.add('hidden'); 
+        arrow.classList.add('arrowUp');       
+    } else {
+        section.classList.add('hidden');
+        formContainer.classList.remove('hidden');
+        btnShare.classList.add('hidden');
+        arrow.classList.remove('arrowUp');
+    }
+
 }
+    
+  
 
 legendDesign.addEventListener("click",handleHiddenDesign);
 
 
 function handleHiddenForm(event) {
-    formContainer.classList.toggle('hidden');
-    arrow2.classList.toggle('arrowUp');
+    if (formContainer.classList.contains('hidden')) {
+        formContainer.classList.remove('hidden');
+        btnShare.classList.add('hidden');
+        section.classList.add('hidden'); 
+        arrow2.classList.add('arrowUp');       
+    } else {
+        formContainer.classList.add('hidden');
+        btnShare.classList.remove('hidden');
+        section.classList.add('hidden');
+        arrow2.classList.remove('arrowUp');
+    }
 }
 
 legendFill.addEventListener("click",handleHiddenForm);
 
 function handleHiddenShare(event) {
-    btnShare.classList.toggle('hidden');
-    arrow3.classList.toggle('arrowUp');
+    if (btnShare.classList.contains('hidden')) {
+        btnShare.classList.remove('hidden');
+        formContainer.classList.add('hidden');
+        section.classList.add('hidden'); 
+        arrow3.classList.add('arrowUp');       
+    } else {
+        btnShare.classList.add('hidden');
+        formContainer.classList.remove('hidden');
+        section.classList.add('hidden');
+        arrow3.classList.remove('arrowUp');
+    }
+   
 }
 
 legendShare.addEventListener("click",handleHiddenShare)
+
+
 
 //crear un evento para todo el form que será el current target//
 /*
@@ -33,3 +66,14 @@ function handleForm(event) {
 form.addEventListener("click", handleForm);
 
 */
+
+
+// function pepino () {
+//     if ()
+
+// }
+
+// const allHeaders = document.querySelectorAll('.js-header-section');
+// for (const header of allHeaders) {
+//     header.addEventListener('click', pepino)
+// }
