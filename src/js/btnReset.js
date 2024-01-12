142fead1bc5a126e2771ab387de220e35a986f30
Wilanda2 <yolanda.lopezdelarica@gmail.com>
2024-01-12 14:04:59 +0100

    Reset button

--- src/js/btnReset.js
+++ src/js/btnReset.js
@@ -1,20 +1,43 @@
 'use strict';
+//3 cosas: 
+//1. Borrar inputs
+//2. 
 
-const btnReset=document.querySelector ('.js-reset-botton');
+
+
+function funcResetObject() {
+    dataForm = {
+        palette: 1,
+        name: "",
+        job: "",
+        phone: "",
+        email: "",
+        linkedin: "",
+        github: "",
+        photo: ""
+};
+
+}
+
+function funcResetPreview() {
+    card.classList.add('preview-palette1');
+    return card;
+}
 
 
 function handleReset (event) {
-    event.preventDefault;
-    card.classList.add('preview-palette1');
-    dataForm ="";
+    const resetPreviewCard = funcResetPreview();
+    const resetObject = funcResetObject();
+    console.log(resetObject);
+    console.log(btnReset);
     
     /*for (const key in dataForm) {
     delete dataForm[key];
     }*/
   
-    console.log(dataForm); 
+    // console.log(dataForm); 
   
-    console.log("you resetted");
+    // console.log("you resetted");
 }
 
 btnReset.addEventListener ('click',handleReset);
